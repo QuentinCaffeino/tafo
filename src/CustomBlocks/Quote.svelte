fe1013d655a3590f756d7e60b9ef2ae0771a6541
Sergei ZH <sergey95aeses@gmail.com>
2019-07-04 23:59:36 +0200

    Made Quoutes custom non-necessary block

--- src/CustomBlocks/Quote.svelte
+++ src/CustomBlocks/Quote.svelte
@@ -1,18 +1,28 @@
 <script>
+    import {fly} from 'svelte/transition';
+
     const api = 'https://api.adviceslip.com/advice';
 
+
+    let visible = false;
+
     let quoteText = '';
+
 
     fetch(api)
             .then(response => {
                 response.json()
                         .then(data => {
                             quoteText = data.slip.advice;
+                            visible = !visible;
                         })
             });
 </script>
 
 
-<div class="mdc-card mdc-card--outlined">
-    <h3>{quoteText}</h3>
-</div>
+{#if visible}
+    <div class="col shadow pt-3 pr-3 pl-3 pb-2 bg-white rounded text-dark"
+         transition:fly="{{ y: 200, duration: 400 }}">
+        <h3>{quoteText}</h3>
+    </div>
+{/if}
