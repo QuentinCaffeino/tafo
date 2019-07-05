10bbcaea864ea3807036aa5628a9c3ca4b0ed053
Sergei ZH <sergey95aeses@gmail.com>
2019-07-05 14:25:23 +0200

    Added animations

--- src/Container/Container.svelte
+++ src/Container/Container.svelte
@@ -1,4 +1,5 @@
 <script>
+    import {fly} from 'svelte/transition';
     import Options from "./Options.svelte";
     import Column from "./Column.svelte";
     import Quote from "../CustomBlocks/Quote.svelte";
@@ -37,16 +38,14 @@
 
         <Column>
             {#if !options}
-                <div class="col shadow pt-3 pr-3 pl-3 pb-2 bg-white rounded text-dark"
+                <div class="horizontally-centered"
                      transition:fly="{{ y: 200, duration: 400 }}">
-                    <div class="horizontally-centered">
-                        <div class="flr">
-                            <div class="row col-12">
-                                <Clock></Clock>
-                            </div>
-                            <div class="row col-12">
-                                <Weather></Weather>
-                            </div>
+                    <div class="flr">
+                        <div class="row col-12">
+                            <Clock></Clock>
+                        </div>
+                        <div class="row col-12">
+                            <Weather></Weather>
                         </div>
                     </div>
                 </div>
